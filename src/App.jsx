c96ae3058f307a2caa54415f8c54a316c9ee78de
Virginia Barbaro <virginiabarbaro@icloud.com>
2023-04-20 17:24:37 -0300

    probando formidable

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import SignupAdmin from "./pages/SignupAdmin";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />;
+        <Route path="/sign-up" element={<SignupAdmin />} />;
       </Routes>
     </>
   );
---
+++ src/pages/SignupAdmin.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function SignupAdmin() {
+  return (
+    <div className="container">
+      <div className="row">
+        <form>
+          <input
+            type="text"
+            placeholder="Firstname"
+            name="firstname"
+            id="firstname"
+          />
+          <input
+            type="text"
+            placeholder="Lastname"
+            name="lastname"
+            id="lastname"
+          />
+          <input
+            type="text"
+            placeholder="Lastname"
+            name="lastname"
+            id="lastname"
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SignupAdmin;
