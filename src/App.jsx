9c1c8a986d2068a737da1c1dab3d140e9a23d9db
Virginia Barbaro <virginiabarbaro@icloud.com>
2023-04-21 16:46:58 -0300

    agregando rutas de login y register

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import SignupAdmin from "./pages/SignupAdmin";
+import CreateAdmin from "./pages/CreateAdmin";
 import Services from "./pages/Services";
+import SignUpCustomer from "./pages/SignUpCustomer";
+import LoginCustomer from "./pages/LoginCustomer";
 
 function App() {
   return (
@@ -10,7 +12,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />;
         <Route path="/services" element={<Services />} />;
-        <Route path="/sign-up" element={<SignupAdmin />} />;
+        <Route path="/create/admin" element={<CreateAdmin />} />;
+        <Route path="/sign-up" element={<SignUpCustomer />} />;
+        <Route path="/login" element={<LoginCustomer />} />;
+        <Route path="*" element={<p>Error 404 page not found!</p>} />
       </Routes>
     </>
   );
---
+++ src/pages/SignUpCustomer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SignUpCustomer() {
+  return <div>Esta es la pagina de registo</div>;
+}
+
+export default SignUpCustomer;
