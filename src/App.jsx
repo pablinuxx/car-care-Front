2359a746fc93a906a846412ab753d599d0502707
Ali <dev.alifm@gmail.com>
2023-04-21 11:28:05 -0300

    Update front

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignupAdmin from "./pages/SignupAdmin";
+import Services from "./pages/Services";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />;
+        <Route path="/services" element={<Services />} />;
         <Route path="/sign-up" element={<SignupAdmin />} />;
       </Routes>
     </>
