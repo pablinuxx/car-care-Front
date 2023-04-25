2d64705b2675c6838f66b1cfa9041d97ddc797b0
Virginia Barbaro <virginiabarbaro@icloud.com>
2023-04-24 22:57:57 -0300

    final login and signup

--- src/pages/LoginCustomer.jsx
+++ src/pages/LoginCustomer.jsx
@@ -19,6 +19,8 @@ function LoginCustomer() {
   const invalidCredentials = () => toast.error("Invalid credentials");
   const userNotFound = () => toast.error("You are not registred yet!");
 
+  const notify = () => toast("Welcome");
+
   const handleUserLogin = async (event) => {
     try {
       event.preventDefault();
@@ -41,6 +43,7 @@ function LoginCustomer() {
         return userNotFound();
       } else {
         dispatch(setLoggedUser(response.data));
+        notify()
         navigate("/");
       }
     } catch (error) {
@@ -50,62 +53,64 @@ function LoginCustomer() {
 
   return (
     <>
-      <div className="background-image"></div>
-      <div className="container">
-        <div className="row">
-          <div className="col-12 col-md-8 col-sm-12 formulary-sign-up p-5 mt-5">
-            <div className="header-logo p-2 w-75 m-auto">
-              <div className="header-text">
-                <h3 className="my-3">Sign in</h3>
-                <p>
-                  Don't have an account?{" "}
-                  <Link to={"/signup"}>
-                    <strong className="sign-in-strong"> SIGN UP</strong>
-                  </Link>
-                </p>
+      <div className="background-image">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-8 col-sm-12 formulary-sign-up p-5 mt-5">
+              <div className="header-logo p-2 w-75 m-auto">
+                <div className="header-text">
+                  <h3 className="my-3">Sign in</h3>
+                  <p>
+                    Don't have an account?{" "}
+                    <Link to={"/signup"}>
+                      <strong className="sign-in-strong"> SIGN UP</strong>
+                    </Link>
+                  </p>
+                </div>
+                <div className="logo">
+                  <img className="logo-sign-up" src="/img/CarCare1.png" />
+                </div>
               </div>
-              <div className="logo">
-                <img className="logo-sign-up" src="/img/CarCare1.png" />
-              </div>
+              <form
+                onSubmit={(event) => handleUserLogin(event)}
+                className="form-sign-up"
+              >
+                <ToastContainer />
+                <div className="form-container w-75 m-auto">
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={email}
+                      onChange={(event) => setEmail(event.target.value)}
+                      className="form-input"
+                      placeholder=" "
+                    />
+                    <label htmlFor="email" className="form-label">
+                      Email
+                    </label>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      value={password}
+                      onChange={(event) => setPassword(event.target.value)}
+                      className="form-input"
+                      placeholder=" "
+                    />
+                    <label htmlFor="email" className="form-label">
+                      Password
+                    </label>
+                  </div>
+                </div>
+                <div className="action-confirm-login mt-3">
+                  <button className="btn-register">Confirm</button>
+                </div>
+              </form>
             </div>
-            <form
-              onSubmit={(event) => handleUserLogin(event)}
-              className="form-sign-up"
-            >
-              <div className="form-container w-75 m-auto">
-                <div className="form-group">
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                    className="form-input"
-                    placeholder=" "
-                  />
-                  <label htmlFor="email" className="form-label">
-                    Email
-                  </label>
-                </div>
-                <div className="form-group">
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                    className="form-input"
-                    placeholder=" "
-                  />
-                  <label htmlFor="email" className="form-label">
-                    Password
-                  </label>
-                </div>
-              </div>
-              <div className="action-confirm-login mt-3">
-                <button className="btn-register">Confirm</button>
-              </div>
-            </form>
           </div>
         </div>
       </div>
--- src/pages/SignUpCustomer.jsx
+++ src/pages/SignUpCustomer.jsx
@@ -42,98 +42,100 @@ function SignUpCustomer() {
   return (
     <>
       <ToastContainer />
-      <div className="container">
-        <div className="row">
-          <div className="col-12 col-md-8 col-sm-12 formulary-sign-up p-4">
-            <div className="header-logo p-2 w-75 m-auto">
-              <div className="header-text">
-                <h3 className="my-3">Sign up</h3>
-                <p>
-                  Already have an account?{" "}
-                  <Link to={"/login"}>
-                    <strong className="sign-in-strong">SIGN IN</strong>
-                  </Link>
-                </p>
+      <div className="background-image">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-8 col-sm-12 formulary-sign-up p-4">
+              <div className="header-logo p-2 w-75 m-auto">
+                <div className="header-text">
+                  <h3 className="my-3">Sign up</h3>
+                  <p>
+                    Already have an account?{" "}
+                    <Link to={"/login"}>
+                      <strong className="sign-in-strong">SIGN IN</strong>
+                    </Link>
+                  </p>
+                </div>
+                <div className="logo">
+                  <img className="logo-sign-up" src="/img/CarCare1.png" />
+                </div>
               </div>
-              <div className="logo">
-                <img className="logo-sign-up" src="/img/CarCare1.png" />
-              </div>
+              <form
+                onSubmit={(event) => handleSubmit(event)}
+                className="form-sign-up"
+              >
+                <div className="form-container w-75 m-auto">
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      id="firstname"
+                      name="firstname"
+                      value={firstname}
+                      onChange={(event) => setFirstname(event.target.value)}
+                      className="form-input"
+                      placeholder=" "
+                    />
+                    <label htmlFor="firstname" className="form-label">
+                      Firstname
+                    </label>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      id="lastname"
+                      name="lastname"
+                      value={lastname}
+                      onChange={(event) => setLastname(event.target.value)}
+                      className="form-input"
+                      placeholder=" "
+                    />
+                    <label htmlFor="lastname" className="form-label">
+                      Lastname
+                    </label>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={email}
+                      onChange={(event) => setEmail(event.target.value)}
+                      className="form-input"
+                      placeholder=" "
+                    />
+                    <label htmlFor="email" className="form-label">
+                      Email
+                    </label>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      value={password}
+                      onChange={(event) => setPassword(event.target.value)}
+                      className="form-input"
+                      placeholder=" "
+                    />
+                    <label htmlFor="email" className="form-label">
+                      Password
+                    </label>
+                  </div>
+                  <div>
+                    <input
+                      type="file"
+                      id="avatar"
+                      name="avatar"
+                      className="avatar-input"
+                      onChange={(event) => setAvatar(event.target.files[0])}
+                    />
+                  </div>
+                </div>
+                <div className="action-confirm-login mt-3">
+                  <button className="btn-register">Confirm</button>
+                </div>
+              </form>
             </div>
-            <form
-              onSubmit={(event) => handleSubmit(event)}
-              className="form-sign-up"
-            >
-              <div className="form-container w-75 m-auto">
-                <div className="form-group">
-                  <input
-                    type="text"
-                    id="firstname"
-                    name="firstname"
-                    value={firstname}
-                    onChange={(event) => setFirstname(event.target.value)}
-                    className="form-input"
-                    placeholder=" "
-                  />
-                  <label htmlFor="firstname" className="form-label">
-                    Firstname
-                  </label>
-                </div>
-                <div className="form-group">
-                  <input
-                    type="text"
-                    id="lastname"
-                    name="lastname"
-                    value={lastname}
-                    onChange={(event) => setLastname(event.target.value)}
-                    className="form-input"
-                    placeholder=" "
-                  />
-                  <label htmlFor="lastname" className="form-label">
-                    Lastname
-                  </label>
-                </div>
-                <div className="form-group">
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                    className="form-input"
-                    placeholder=" "
-                  />
-                  <label htmlFor="email" className="form-label">
-                    Email
-                  </label>
-                </div>
-                <div className="form-group">
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                    className="form-input"
-                    placeholder=" "
-                  />
-                  <label htmlFor="email" className="form-label">
-                    Password
-                  </label>
-                </div>
-                <div>
-                  <input
-                    type="file"
-                    id="avatar"
-                    name="avatar"
-                    className="avatar-input"
-                    onChange={(event) => setAvatar(event.target.files[0])}
-                  />
-                </div>
-              </div>
-              <div className="action-confirm-login mt-3">
-                <button className="btn-register">Confirm</button>
-              </div>
-            </form>
           </div>
         </div>
       </div>
