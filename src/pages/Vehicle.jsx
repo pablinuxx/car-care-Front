3b6fbc928ca80b7a5a1411130839f129dd648946
juan-abraham-dv <123184630+juan-abraham-dv@users.noreply.github.com>
2023-04-26 18:48:05 -0300

    Update car, effect in photo

--- src/pages/Vehicle.jsx
+++ src/pages/Vehicle.jsx
@@ -27,41 +27,47 @@ function Vehicle() {
       <Navbar />
       <div className="container-fluid">
         <div className="row pb-0">
-          <div className="col  g-0">
-            <img
-              src={`${import.meta.env.VITE_APP_API_URL}/${
-                car ? car.image[0] : ""
-              }`}
-              className="hero-front-image"
-              alt={car && car.name}
-            />
+          <div className="col car-shine g-0">
+            <figure className="p-0 m-0">
+              <img
+                src={`${import.meta.env.VITE_APP_API_URL}/${
+                  car ? car.image[0] : ""
+                }`}
+                className="hero-front-image"
+                alt={car && car.name}
+              />{" "}
+            </figure>
           </div>
         </div>
-        <div className="row">
-          <div className="col  g-0">
-            <img
-              src={`${import.meta.env.VITE_APP_API_URL}/${
-                car ? car.image[2] : ""
-              }`}
-              className="hero-front-image"
-              alt={car && car.name}
-            />
+        <div className="row ">
+          <div className="col-6  g-0">
+            <figure className="p-0 m-0 ">
+              <img
+                src={`${import.meta.env.VITE_APP_API_URL}/${
+                  car ? car.image[2] : ""
+                }`}
+                className="hero-front-image"
+                alt={car && car.name}
+              />{" "}
+            </figure>
           </div>
           <div className="col-6 information-box text-white d-flex flex-column align-items-start justify-content-start g-0">
-            <h1 className="fw-bold fs-1 ms-4 mt-5">{car && car.name}</h1>
-            <h3 className="fs-3 ms-4 mt-1">{car && car.description}</h3>
-            <h2 className="fw-bold fs-1 ms-4 mt-2">USD {car && car.price}</h2>
+            <h1 className="fw-bold fs-2 ms-4 mt-5">{car && car.name}</h1>
+            <h3 className="fs-4 ms-4 mt-1">{car && car.description}</h3>
+            <h2 className="fw-bold fs-2 ms-4 mt-2">USD {car && car.price}</h2>
           </div>
         </div>
         <div className="row">
-          <div className="col g-0">
-            <img
-              src={`${import.meta.env.VITE_APP_API_URL}/${
-                car ? car.image[1] : ""
-              }`}
-              className="hero-front-image"
-              alt={car && car.name}
-            />
+          <div className="col car-shine g-0">
+            <figure className="p-0 m-0">
+              <img
+                src={`${import.meta.env.VITE_APP_API_URL}/${
+                  car ? car.image[1] : ""
+                }`}
+                className="hero-front-image"
+                alt={car && car.name}
+              />{" "}
+            </figure>
           </div>
         </div>
       </div>
