6031b66495e498e1a774295a4096dd0b63c15276
Virginia Barbaro <virginiabarbaro@icloud.com>
2023-05-02 10:26:34 -0300

    crud customers

--- src/pages/adminPanelPage/AddCustomer.jsx
+++ src/pages/adminPanelPage/AddCustomer.jsx
@@ -1,9 +1,136 @@
-import React from "react";
+import React, { useState } from "react";
+import NavbarAdmin from "../../components/adminPanel/NavbarAdmin";
+import SidebarAdmin from "../../components/adminPanel/SidebarAdmin";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 
 function AddCustomer() {
-  return <div>
-    
-  </div>;
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [avatar, setAvatar] = useState("");
+
+  const user = useSelector((state) => state.session);
+  const navigate = useNavigate();
+
+  const handleAddCustomer = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("firstname", firstname);
+    formData.append("lastname", lastname);
+    formData.append("email", email);
+    formData.append("password", password);
+    formData.append("avatar", avatar);
+
+    const response = await axios({
+      headers: {
+        // Authorization: `bearer: ${user.token}`,
+        "Content-Type": "multipart/form-data",
+      },
+      method: "post",
+      url: `${import.meta.env.VITE_APP_API_URL}/customers`,
+      data: formData,
+    });
+
+    navigate("/admin/customers");
+  };
+
+  return (
+    <>
+      <NavbarAdmin />
+      <div className="d-flex">
+        <SidebarAdmin />
+        <div className="container mt-3 d-flex">
+          <form
+            className="form-sign-up w-75 mx-auto"
+            onSubmit={(e) => handleAddCustomer(e)}
+          >
+            <h2 className="text-center p-3">Create new customer</h2>
+            <div className="form-container w-75 m-auto">
+              <div className="form-group">
+                <input
+                  type="text"
+                  id="firstname"
+                  name="firstname"
+                  value={firstname}
+                  onChange={(e) => setFirstname(e.target.value)}
+                  className="form-input text-dark"
+                  placeholder=" "
+                  required
+                />
+                <label htmlFor="firstname" className="form-label-admin">
+                  firstname
+                </label>
+              </div>
+              <div className="form-group">
+                <input
+                  required
+                  type="text"
+                  id="lastname"
+                  name="lastname"
+                  value={lastname}
+                  onChange={(e) => setLastname(e.target.value)}
+                  className="form-input text-dark"
+                  placeholder=" "
+                />
+                <label htmlFor="lastname" className="form-label-admin">
+                  Lastname
+                </label>
+              </div>
+              <div className="form-group">
+                <input
+                  required
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="form-input text-dark"
+                  placeholder=" "
+                />
+                <label htmlFor="email" className="form-label-admin">
+                  Email
+                </label>
+              </div>
+
+              <div className="form-group">
+                <input
+                  required
+                  type="password"
+                  id="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="form-input text-dark"
+                  placeholder=" "
+                />
+                <label htmlFor="password" className="form-label-admin">
+                  Password
+                </label>
+              </div>
+
+              <div className="form-group">
+                <input
+                  required
+                  type="file"
+                  id="avatar"
+                  name="avatar"
+                  onChange={(e) => setAvatar(e.target.files[0])}
+                />
+                <label htmlFor="avatar"></label>
+              </div>
+            </div>
+            <div className="action-confirm-login mt-4 mb-5">
+              <button className="btn-register text-white ">Create</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AddCustomer;
--- src/pages/adminPanelPage/AdminCustomers.jsx
+++ src/pages/adminPanelPage/AdminCustomers.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import NavbarAdmin from "../../components/adminPanel/NavbarAdmin";
 import SidebarAdmin from "../../components/adminPanel/SidebarAdmin";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Avatar } from "@chakra-ui/react";
 import {
@@ -24,10 +24,24 @@ import {
 //   ModalCloseButton,
 // } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react";
+import { ToastContainer } from "react-toastify";
 
 function AdminCustomers() {
-  const loggedUser = useSelector((state) => state.session);
+  const user = useSelector((state) => state.session);
+  const { id } = useParams();
   const [customers, setCustomers] = useState([]);
+
+  const notify = () =>
+    toast.success("Successfully deleted!", {
+      position: "bottom-center",
+      autoClose: 4000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
 
   useEffect(() => {
     const getCustomers = async () => {
@@ -38,11 +52,25 @@ function AdminCustomers() {
         method: "get",
         url: `${import.meta.env.VITE_APP_API_URL}/customers`,
       });
-      console.log(response.data);
       setCustomers(response.data);
     };
     getCustomers();
   }, []);
+
+  const handleDeleteCustomer = async (id) => {
+    try {
+      setCustomers(customers.filter((customer) => customer.id !== id));
+      await axios({
+        // headers: {
+        //   Authorization: `bearer: ${user.token}`,
+        // },
+        method: "delete",
+        url: `${import.meta.env.VITE_APP_API_URL}/customers/${id}`,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     customers && (
@@ -53,7 +81,7 @@ function AdminCustomers() {
           <section className="container p-4">
             <div className="d-flex justify-content-between">
               <h2>Customers</h2>
-              <Link to="/admin/add/services">
+              <Link to="/admin/add/customers">
                 <Button className="btn-add-tables" variant="outline">
                   <i className="bi bi-plus-circle me-3"></i> Add customers
                 </Button>
@@ -75,8 +103,8 @@ function AdminCustomers() {
                   {customers.map((customer) => {
                     return (
                       <>
-                        <Tr key={customer.id}>
-                          <Td>{customer.id}</Td>
+                        <Tr>
+                          <Td key={customer.id}>{customer.id}</Td>
                           <Td>
                             <Avatar
                               src={
@@ -99,31 +127,15 @@ function AdminCustomers() {
                             <Link to={`/admin/edit/customers/${customer.id}`}>
                               <i className="bi bi-pencil-square mx-2 icon-modify-panel-admin"></i>
                             </Link>
-                            <Button
-                              // onClick={onOpen}
-                              className="btn-open-modal-delete"
-                            >
-                              <i className="bi bi-trash3 mx-2 icon-delete-panel-admin"></i>
+                            <Button className="btn-open-modal-delete">
+                              <i
+                                className="bi bi-trash3 mx-2 icon-delete-panel-admin"
+                                onClick={() =>
+                                  handleDeleteCustomer(customer.id)
+                                }
+                              ></i>
                             </Button>
-                            {/* <Modal isOpen={isOpen} onClose={onClose}>
-                              <ModalOverlay />
-                              <ModalContent>
-                                <ModalHeader>Delete service</ModalHeader>
-                                <ModalCloseButton />
-                                <ModalBody>
-                                  Are you sure you want to delete this service?
-                                </ModalBody>
-
-                                <ModalFooter>
-                                  <Button
-                                    className="confirm-delete"
-                                    onClick={() => handleDeleteService(id)}
-                                  >
-                                    Delete
-                                  </Button>
-                                </ModalFooter>
-                              </ModalContent>
-                            </Modal> */}
+                            <ToastContainer />
                           </Td>
                         </Tr>
                       </>
