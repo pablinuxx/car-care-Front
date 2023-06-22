466402c4f491ba9795383ba77c4c3ccbb38db44e
VirginiaBarbaro <virginiabarbaro@icloud.com>
2023-06-22 17:50:06 -0300

    scroll to level

--- src/pages/Brand.jsx
+++ src/pages/Brand.jsx
@@ -6,6 +6,10 @@ import axios from "axios";
 import { Card, Heading, Divider, CardBody, Button } from "@chakra-ui/react";
 
 const Brand = () => {
+  const handleClick = () => {
+    window.scrollTo(0, 0);
+  };
+
   const { name } = useParams();
 
   const [brands, setBrands] = useState([]);
@@ -61,7 +65,7 @@ const Brand = () => {
                         </Heading>
                         <h6>From {brand.price} USD </h6>
                         <Divider />
-                        <Link to={`/vehicle/${brand.id}`}>
+                        <Link to={`/vehicle/${brand.id}`} onClick={handleClick}>
                           <Button variant="link" className="discovery-btn p-2">
                             Discovery
                             <i className=" bi bi-arrow-right ms-2"></i>
